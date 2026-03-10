57be0450be93cfed1c5b5e08bcc890dcdbaa6760
ToojoNariv <tojomanambina@gmail.com>
2026-03-10 10:24:17 +0300

    suppression des fichier de test

--- debug_models.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach (glob('app/Models/*.php') as $filename) {
-    $content = file_get_contents($filename);
-    if (preg_match('/protected \$table = [\'"](.+?)[\'"];/', $content, $matches)) {
-        echo basename($filename) . ": " . $matches[1] . "\n";
-    } else {
-         echo basename($filename) . ": [default]\n";
-    }
-}
